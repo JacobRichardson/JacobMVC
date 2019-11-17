35c45ac9f2b411a5b024852000b254c280560314
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2019-11-17 13:45:42 -0500

    Database class query.
    This commit finishes the Database class to allow queries to be
    made to the database as well as return the data from the
    database.

--- app/lib/database.php
+++ app/lib/database.php
@@ -45,5 +45,74 @@
                 echo $this->error;
             }
         }
+
+        // Query method.
+        public function query ($sql) {
+
+            // Set the statement on the class equal to the prepared sql.
+            $this->stmt = $this->dbh->prepare($sql);
+        }
+
+        // Bind method.
+        public function bind ($param, $value, $type = null) {
+
+            // If type is null.
+            if (is_null($type)) {
+
+                // If type is an int set it to an int else null.
+                $type = is_int($value) ? PDO::PARAM_INT : null;
+
+                // If type is an bool set it to an bool else null.
+                $type = is_bool($vaule) ? PDO::PARAM_BOOL : null;
+
+                // If type is an null set it to an null else null.
+                $type = is_null($value) ? PDO::PARAM_NULL : null;
+
+                // If the type is still null.
+                if (is_null($type)) {
+
+                    // Default to a string.
+                    $type = PDO::PARAM_STRING;
+                }
+            }
+
+            // Bind the values to the query.
+            $this->stmt->bindValue($param, $value, $type);
+        }
+
+        // Execute method.
+        public function execute () {
+
+            // Return the result of executing the statement.
+            return $this->stmt->execute();
+        }
+
+        // Result set method.
+        public function resultSet () {
+
+            // Invoke the class method execute.
+            $this->execute();
+
+            // Return the result of fetching all results of the statement.
+            return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+        }
+
+        // Single method.
+        public function single () {
+
+            // Invoke the class method execute.
+            $this->execute();
+
+            // Return the result of fetching a single result of the statement.
+            return $this->stmt->fetch(PDO::FETCH_OBJ);
+        }
+
+        // Row count method.
+        public function rowCount () {
+
+            // Return the statement's row count.
+            return $this->stmt->rowCount();
+        }
+
     }
 ?>
