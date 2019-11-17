fb369b1d5f2f0b7c1ea3dc679b7d59c1da5c603c
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2019-11-16 23:41:00 -0500

    Core params.
    This commit updates the core controller to pass the params
    to the method of the controller.

--- app/lib/core.php
+++ app/lib/core.php
@@ -35,6 +35,22 @@
 
             // Instantiate the specified controller.
             $this->currentController = new $this->currentController;
+
+            // If a method is there and the method exists on the controller.
+            if (isset($url[1]) && method_exists($this->currentController, $url[1])) {
+
+                // Set the current method equal to the specified method.
+                $this->currentMethod = $url[1];
+
+                // Remove the method from the url.
+                unset($url[1]);
+            }
+
+            // If url is defined set the params if not set params to an empty array.
+            $this->params = $url ? array_values($url) : [];
+
+            // Call the method with the params.
+            call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
         }
 
         // Get url method.
