0207a7b98b8004bf6edef050bbe737865f9d3d0f
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2019-11-17 00:03:59 -0500

    Config.php.
    This commit creates the config.php file. This file contains a
    couple of constants.

---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+    // App root.
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    // URL root.
+    define('URLROOT', 'http://localhost/jacobmvc');
+
+    // Site name.
+    define('SITENAME', 'JacobMVC');
+?>
